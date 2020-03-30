437cb48a62de475c7425a9a54072059cc9ed83f8
Julian Fischer <julian.fischer@anynines.com>
2020-03-30 14:11:46 +0200

    Removes .docosaurus

--- website/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("infima/dist/css/default/default.css"),
-  require("remark-admonitions/styles/infima.css"),
-  require("/Users/mariusz/Documents/work/a9s-kubernetes-training/website/src/css/custom.css"),
-  require("/Users/mariusz/Documents/work/a9s-kubernetes-training/website/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
-];
