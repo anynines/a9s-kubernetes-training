c8312b0d4be6bd708199eb7f029c06885527f18b
Julian Fischer <julian.fischer@anynines.com>
2020-04-01 18:24:15 +0200

    Testing an alt docusaurus conf

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -2,9 +2,9 @@ const siteConfig = {
   title: 'Kubernetes Training', // Title for your website.
   tagline: 'Learn how to containerize and deploy your apps to Kubernetes.',
   url: 'https://anynines.github.io', //'https://learn.anynines.com/k8s', // Your website URL
-  baseUrl: '/', // '/a9s-kubernetes-training/' // Base URL for your project */
+  baseUrl: '/a9s-kubernetes-training/', // '/' // Base URL for your project */
   favicon: 'img/favicon.ico',
-  projectName: 'anynines.github.io', // a9s-kubernetes-training
+  projectName: 'a9s-kubernetes-training', // anynines.github.io
   organizationName: 'anynines',
 
   themeConfig: {
