49b2fe75f865eb1b01e2a5bd1ef5c8164634caf8
Matthias Folz <folz@avarteq.de>
2021-10-29 11:47:36 +0200

    changes for PR

--- website/src/theme/VideoContainer.js
+++ website/src/theme/VideoContainer.js
@@ -1,53 +1,6 @@
 import React from 'react'
-import { Divider, Grid, Typography } from '@anynines/a9s-design-system'
-
-function isDarkModeActive() {
-  return document.querySelectorAll('html[data-theme="dark"]').length > 0
-}
-
-const Video = ({ src, title, size, faded }) => {
-  let klass
-  if (faded) {
-    klass = 'fadedVideo'
-  } else {
-    title = null
-  }
-  return (
-    <Grid item lg={size.lg} sm={size.sm} xs={12}>
-      <div
-        style={{
-          paddingBottom: '56.25%',
-          paddingTop: '0',
-          position: 'relative',
-          height: '0',
-          borderRadius: 4,
-          overflow: 'hidden',
-          marginBottom: 14
-        }}
-      >
-        <iframe
-          className={klass}
-          style={{
-            width: '100%',
-            height: '100%',
-            top: '0',
-            left: '0',
-            position: 'absolute'
-          }}
-          width='320'
-          src={src + '?rel=0&modestbranding=1&playsinline=0&controls=2'}
-          title='YouTube video player'
-          frameBorder='0'
-          allow='accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-          allowFullScreen
-        ></iframe>
-      </div>
-      <Typography fontWeight='800' variant='subtitle2'>
-        {title}
-      </Typography>
-    </Grid>
-  )
-}
+import { Divider, Grid } from '@anynines/a9s-design-system'
+import Video from './Video.js'
 
 export default function VideoContainer({ title, list }) {
   let size, faded
